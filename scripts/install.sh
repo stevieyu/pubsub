af0899881e445ad2dd6648e42ce6a8e6cf65d040
Your Name <you@example.com>
2024-09-06 11:06:55 +0000

    3434

---
+++ scripts/install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+url="https://www.ghproxy.cn/https://github.com/dunglas/frankenphp/releases/latest/download/frankenphp-linux-x86_64" && \
+binpath="/root/.local/bin" && \
+curl -L "$url" -o "$binpath"/frankenphp && \
+chmod +x "$binpath"/frankenphp && \
+echo '#!/usr/bin/env bash' > "$binpath"/php && \
+echo 'frankenphp php-cli "$@"' >> "$binpath"/php && \
+chmod +x "$binpath"/php && \
+curl -L https://mirrors.aliyun.com/composer/composer.phar -o "$binpath"/composer && \
+chmod +x "$binpath"/composer && \
+composer config -g repos.packagist composer https://mirrors.tencent.com/composer/
